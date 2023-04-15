eb2abd17d0c736c37be50b0ae7e048fa849953d3
Joao Gomes <joaopfg.2002@gmail.com>
2023-04-15 19:23:27 +0100

    Add colomnline

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,8 +4,8 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
 }
